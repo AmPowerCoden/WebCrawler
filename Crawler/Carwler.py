eef7f39786178863db314bb0bc236d4a6f2f0326
AmPowerCoden <102946178+AmPowerCoden@users.noreply.github.com>
2023-06-07 14:45:54 +0200

    test

--- Crawler/Carwler.py
+++ Crawler/Carwler.py
@@ -1,1 +1,18 @@
 import requests
+import re
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+    URL = "https://de.wikipedia.org/wiki/Astronomische_Koordinatensysteme#Relative_Koordinatensysteme"
+    resp = requests.get(URL)
+    print(resp.status_code)
+    print(resp.text)
+    soup = BeautifulSoup(resp.text)
+
+    x = soup.findAll('a', attrs={'href': re.compile("^http://")})
+
+    regex_pattern = r'\s+<a href="(.*?)"'
+
+    print(re.findall(regex_pattern, resp.text))
+
+    regex = re.findall
